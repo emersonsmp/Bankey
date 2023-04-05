429981acf5899e089a64fb395713552eb9d9b9f4
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2023-04-05 14:56:21 -0300

    style balance value

--- Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Utils/UIViewController+Utils.swift
@@ -9,22 +9,22 @@ import UIKit
 
 extension UIViewController {
     
-    func setStatusbar(){
-        let statusBarSize = UIApplication.shared.statusBarFrame.size
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusBarView = UIView(frame: frame)
-
-        statusBarView.backgroundColor = appColor
-        view.addSubview(statusBarView)
-    }
-    
-//    func setStatusBar() {
-//        let navBarAppearance = UINavigationBarAppearance()
-//        navBarAppearance.configureWithTransparentBackground()
-//        navBarAppearance.backgroundColor = appColor
-//        UINavigationBar.appearance().standardAppearance = navBarAppearance
-//        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+//    func setStatusbar(){
+//        let statusBarSize = UIApplication.shared.statusBarFrame.size
+//        let frame = CGRect(origin: .zero, size: statusBarSize)
+//        let statusBarView = UIView(frame: frame)
+//
+//        statusBarView.backgroundColor = appColor
+//        view.addSubview(statusBarView)
 //    }
+    
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground()
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
     
     func setTabBarImage(imageName: String, title: String){
         let configuration = UIImage.SymbolConfiguration(scale: .large)
