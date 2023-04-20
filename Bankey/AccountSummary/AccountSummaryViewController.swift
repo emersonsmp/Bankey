b897845433228f1a2e4271f02577023153d673c0
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2023-04-20 17:16:14 -0300

    group account summary network call

--- Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -32,16 +32,12 @@ class AccountSummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        setupNavigationBar()
-    }
-    
-    func setupNavigationBar() {
-        navigationItem.rightBarButtonItem = logoutBarButtonItem
     }
 }
 
 extension AccountSummaryViewController {
     private func setup() {
+        setupNavigationBar()
         setupTableView()
         setupTableHeaderView()
         fetchData()
@@ -75,6 +71,10 @@ extension AccountSummaryViewController {
         headerView.frame.size = size
         
         tableView.tableHeaderView = headerView
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = logoutBarButtonItem
     }
 }
 
@@ -115,27 +115,37 @@ extension AccountSummaryViewController {
 
 extension AccountSummaryViewController {
     private func fetchDataAndLoadViews() {
+        let group = DispatchGroup()
         
+        group.enter()
         fetchProfile(forUserId: "1") { result in
             switch result {
                 case .success(let profile):
                     self.profile = profile
                     self.configureTableHeaderView(with: profile)
-                    self.tableView.reloadData()
+                    //self.tableView.reloadData()
                 case .failure(let error):
                     print(error.localizedDescription)
             }
+            group.leave()
         }
         
+        group.enter()
         fetchAccounts(forUserId: "1") { result in
             switch result {
                 case .success(let accounts):
                     self.accounts = accounts
                     self.configureTableCell(with: accounts)
-                    self.tableView.reloadData()
+                    //self.tableView.reloadData()
                 case .failure(let error):
                     print(error.localizedDescription)
             }
+            
+            group.leave()
+        }
+        
+        group.notify(queue: .main){
+            self.tableView.reloadData()
         }
     }
     
